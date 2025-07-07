abd06b4792296607308c132eac6cdf9dc305d62f
Brando Rocha <brandorocha00@gmail.com>
2025-07-07 20:31:57 -0300

    feat(adapter): add http handler

---
+++ internal/adapters/handler/http.go
@@ -0,0 +1,33 @@
+
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"price-comparator-api/internal/core/ports"
+)
+
+type HTTPHandler struct {
+	service ports.PriceComparatorService
+}
+
+func NewHTTPHandler(service ports.PriceComparatorService) *HTTPHandler {
+	return &HTTPHandler{service: service}
+}
+
+func (h *HTTPHandler) Compare(w http.ResponseWriter, r *http.Request) {
+	product := r.URL.Query().Get("product")
+	if product == "" {
+		http.Error(w, "Product parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	prices, err := h.service.Compare(product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(prices)
+}
