2e2c8bc07a1054674c01aa6d16463a20e59d62a3
Brando Rocha <brandorocha00@gmail.com>
2025-07-12 10:49:19 -0300

    feat: create fx modules for adapters and service

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,24 +5,20 @@ import (
 	"fmt"
 	"net/http"
 
-	"price-comparator-api/internal/domain"
-	"price-comparator-api/internal/ports"
-	"price-comparator-api/internal/service"
 	"price-comparator-api/adapters/handler"
 	"price-comparator-api/adapters/repository"
 	"price-comparator-api/adapters/routes"
+	"price-comparator-api/internal/service"
 
 	"go.uber.org/fx"
 )
 
 func main() {
 	fx.New(
-		fx.Provide(
-			repository.NewSerpAPIRepository,
-			service.NewPriceComparator,
-			handler.NewHTTPHandler,
-			routes.NewMux,
-		),
+		handler.Module,
+		repository.Module,
+		routes.Module,
+		service.Module,
 		fx.Invoke(func(mux *http.ServeMux) {
 			fmt.Println("Server is running on port 8080")
 			http.ListenAndServe(":8080", mux)
