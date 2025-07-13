18fb22e447b5c9c06d762647fc468544142705fd
Brando Rocha <brandorocha00@gmail.com>
2025-07-13 11:36:59 -0300

    feat: change the listen and server

--- adapters/routes/module.go
+++ adapters/routes/module.go
@@ -1,7 +1,18 @@
 package routes
 
-import "go.uber.org/fx"
+import (
+	"fmt"
+	"net/http"
+
+	"go.uber.org/fx"
+)
 
 var Module = fx.Module("routes",
 	fx.Provide(NewMux),
+	fx.Invoke(StartServe),
 )
+
+func StartServe(mux *http.Serve) {
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", mux)
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-
 	"price-comparator-api/adapters/handler"
 	"price-comparator-api/adapters/repository"
 	"price-comparator-api/adapters/routes"
@@ -19,9 +15,5 @@ func main() {
 		repository.Module,
 		routes.Module,
 		service.Module,
-		fx.Invoke(func(mux *http.ServeMux) {
-			fmt.Println("Server is running on port 8080")
-			http.ListenAndServe(":8080", mux)
-		}),
 	).Run()
 }
