8787452a7987eae668c0e310dcddbabd6ac76fae
Brando Rocha <brandorocha00@gmail.com>
2025-07-21 20:05:20 -0300

    feat: add health check

--- adapters/api/routes/index.go
+++ adapters/api/routes/index.go
@@ -3,11 +3,13 @@ package routes
 import (
 	"net/http"
 
-	"price-comparator-api/adapters/handler"
+	"price-comparator-api/adapters/api/handler"
+	"price-comparator-api/adapters/api/health"
 )
 
 func NewMux(h *handler.HTTPHandler) *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/compare", h.Compare)
+	health.HealthRoutes(mux)
 	return mux
 }
