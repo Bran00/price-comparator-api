e954ccaa2e3d6f5d3cc763eeb803a371a530db1a
Brando Rocha <brandorocha00@gmail.com>
2025-07-28 21:07:13 -0300

    fix: start the changes into the files part 2

--- adapters/api/controllers/product/product.go
+++ adapters/api/controllers/product/product.go
@@ -1,20 +1,9 @@
-// Package product has the logic of business
 package product
 
-import (
-	"net/http"
-
-	"price-comparator-api/adapters/api/handler/product"
-)
-
-type ProductController struct {
-	httpHandler *product.HTTPHandler
-}
-
-func NewController(httpHandler *product.HTTPHandler) *Controller {
+func NewProductController(httpHandler *product.HTTPHandler) *Controller {
 	return &Controller{httpHandler: httpHandler}
 }
 
-func (c *Controller) History(w http.ResponseWriter, r *http.Request) {
-	c.httpHandler.Compare(w, r)
+func (c *Controller) HistoryProduct(w http.ResponseWriter, r *http.Request) {
+	
 }
