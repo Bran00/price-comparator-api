b42955e15bb1190c9dae63f9a4a17591d64287d9
Brando Rocha <brandorocha00@gmail.com>
2025-08-07 21:03:13 -0300

    feat: add product usecase

--- internal/product/usecase/product.go
+++ internal/product/usecase/product.go
@@ -1,24 +1,20 @@
 package usecase
 
-import "price-comparator-api/internal/product/ports"
+import (
+	"price-comparator-api/internal/product/domain"
+	"price-comparator-api/internal/product/ports"
+)
 
-type ProductUseCase struct {
-   repository ports.ProductHistoryRepositoryImplemetation
+type Product struct {
+	repository ports.ProductHistoryRepositoryImplemetation
 }
 
-func NewProductUseCase(repository ports.ProductHistoryRepositoryImplemetation) *ProductUseCase {
-  return &ProductUseCase{repository}
+func NewProductUsecase(repository ports.ProductHistoryRepositoryImplemetation) *Product {
+	return &Product{
+		repository: repository,
+	}
 }
 
-func (s *PriceComparator) HistoryProduct(productName string) (do, error) {
-   product := domain.Product{
-    Name: productName, 
-   }
-
-   foundProduct, err := s.repo.FindPrices(product)
-   if err != nil {
-     return nil, err
-   }
-
-   return foundProduct, nil
+func (p *Product) ProductHistory(product string) (domain.Product, error) {
+	return p.repository.ProductHistory(product)
 }
