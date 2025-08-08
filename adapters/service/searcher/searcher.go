a8d2f13d5b831472edd0c8ae273213069e478900
Brando Rocha <brandorocha00@gmail.com>
2025-08-07 21:04:20 -0300

    fix: correct searcher service implementation

--- adapters/service/searcher/searcher.go
+++ adapters/service/searcher/searcher.go
@@ -1,5 +1,17 @@
 package searcher
 
-type searcherServiceImpl {
-  impl  &p
+import (
+	"price-comparator-api/internal/product/domain"
+	"price-comparator-api/internal/product/ports"
+)
+
+type Service struct{}
+
+func NewSearcherService() ports.ProductHistoryRepositoryImplemetation {
+	return &Service{}
+}
+
+func (s *Service) ProductHistory(product string) (domain.Product, error) {
+	// TODO: implement searcher
+	return domain.Product{}, nil
 }
