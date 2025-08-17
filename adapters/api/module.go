08afaa523f738d20821251203db6de1e0a721deb
Brando Rocha <brandorocha00@gmail.com>
2025-08-17 11:58:20 -0300

    feat: adding lifecycle to hook start server

--- adapters/api/module.go
+++ adapters/api/module.go
@@ -1,6 +1,12 @@
 package api
 
-import "go.uber.org/fx"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/fx"
+)
 
 var Module = fx.Module("api", 
   fx.Provide(
@@ -9,4 +15,11 @@ var Module = fx.Module("api",
   fx.Invoke(),
 )
 
+func startServer(lc fx.Lifecycle, router *gin.Engine) {
+  server := &http.Server{
+    Addr: fmt.Sprintf(":%s", 5000),
+    Handler: router,
+  }
 
+  lc.Append(fx.Hook)
+}
